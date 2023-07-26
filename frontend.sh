671f83cd90f5932b9fea081a3b2ffaaa9ec42305
raghav <phpngdeveloper@gmail.com>
2023-07-26 10:53:48 +0530

    commited

--- frontend.sh
+++ frontend.sh
@@ -4,12 +4,18 @@ yum install nginx -y
 
 if [ $? -ne 0 ]; then
 
-echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
-exit $?
-
+    echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
+    exit $?
 fi
 
 echo -e "\e[32mInstallation sucess \e[0m"
 
+echo -e "\e[33mNginx process steps starts\e[0m"
+systemctl enable nginx
+systemctl restart nginx
+if [ $? -ne 0 ]; then
 
-
+    echo -e "\e[31mNginx failed to run \e[0m"
+    exit $?
+fi
+echo -e "\e[32mNginx running\e[0m"
