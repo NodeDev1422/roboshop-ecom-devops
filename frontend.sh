7943630a80803a131fa81875e2a8de99cfd9eb0d
raghav <phpngdeveloper@gmail.com>
2023-07-26 22:00:49 +0530

    started

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,46 @@
-echo -e "\e[33mFrontend Installation started \e[0m"
-
-yum install nginx -y
-
+function getErrMsg()
+{
 if [ $? -ne 0 ]; then
 
-    echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
+    echo -e "\e[33m$1\e[0m"
     exit $?
 fi
 
+echo -e "\e[32$1 \e[0m"
+}
 
-echo -e "\e[32mInstallation sucess \e[0m"
+ getErrMsg "Frontend Installation started"
 
-echo -e "\e[33mNginx process steps starts\e[0m"
-systemctl enable nginx
-systemctl restart nginx
-if [ $? -ne 0 ]; then
+# yum install nginx -y
 
-    echo -e "\e[31mNginx failed to run \e[0m"
-    exit $?
-fi
-echo -e "\e[32mNginx running\e[0m"
+# if [ $? -ne 0 ]; then
+
+#     echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
+#     exit $?
+# fi
+
+# echo -e "\e[33mRemoving existing files in directory\e[0m"
+
+# rm -rf /usr/share/nginx/html/*
+
+# if [ $? -ne 0 ]; then
+
+# echo -e "\e[31m Issue in removing files\e[0m"
+
+# fi
+
+# echo -e "\e[32mRemoved files in directory\e[0m"
+
+
+# echo -e "\e[32mInstallation sucess \e[0m"
+
+# getErrMsg "Nginx process steps starts"
+
+# systemctl enable nginx
+# systemctl restart nginx
+
+
+# getErrMsg "Nginx running"
+
+
+
