8f4f3bd91edea269615f8d3e0a37e4c05aba742f
raghav <phpngdeveloper@gmail.com>
2023-07-26 22:02:58 +0530

    new

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+ source ./functions.sh
  getErrMsg "Frontend Installation started"
 
 # yum install nginx -y
@@ -32,16 +33,7 @@
 # getErrMsg "Nginx running"
 
 
-function getErrMsg()
-{
-if [ $? -ne 0 ]; then
 
-    echo -e "\e[33m$1\e[0m"
-    exit $?
-fi
-
-echo -e "\e[32$1 \e[0m"
-}
 
 
 
---
+++ functions.sh
@@ -0,0 +1,10 @@
+function getErrMsg()
+{
+if [ $? -ne 0 ]; then
+
+    echo -e "\e[33m$1\e[0m"
+    exit $?
+fi
+
+echo -e "\e[32$1 \e[0m"
+}
