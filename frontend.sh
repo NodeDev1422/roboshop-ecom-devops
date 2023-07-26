e31818fe02cd8377c88df0c32d8d52d7ded19571
raghav <phpngdeveloper@gmail.com>
2023-07-26 10:26:18 +0530

    changes

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@ echo -e "\e[33mInstallation started\e[0m"
 
 yum install nginxxxx -y
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
 
     echo -e "\e[31mInstallationn failed with error: $?\e[0m"
 fi
