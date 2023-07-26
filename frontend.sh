cc341fb67df7c725f6061c7867431df0819380cb
raghav <phpngdeveloper@gmail.com>
2023-07-26 22:08:53 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,39 +1,31 @@
- #source ./functions.sh
- echo  "Frontend Installation started"
+echo -e "\e[33mFrontend Installation started \e[0m"
 
-# yum install nginx -y
+yum install nginx -y
 
-# if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
 
-#     echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
-#     exit $?
-# fi
-
-# echo -e "\e[33mRemoving existing files in directory\e[0m"
-
-# rm -rf /usr/share/nginx/html/*
-
-# if [ $? -ne 0 ]; then
-
-# echo -e "\e[31m Issue in removing files\e[0m"
-
-# fi
-
-# echo -e "\e[32mRemoved files in directory\e[0m"
+    echo -e "\e[31mInstallation Failed with error code: $? \e[0m"
+    exit $?
+fi
 
 
-# echo -e "\e[32mInstallation sucess \e[0m"
+remove -rf /usr/share/nginx/html/*
 
-# getErrMsg "Nginx process steps starts"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-# systemctl enable nginx
-# systemctl restart nginx
+cd /usr/share/nginx/html
 
-
-# getErrMsg "Nginx running"
+unzip /tmp/frontend.zip
 
 
+echo -e "\e[32mInstallation sucess \e[0m"
 
+echo -e "\e[33mNginx process steps starts\e[0m"
+systemctl enable nginx
+systemctl restart nginx
+if [ $? -ne 0 ]; then
 
-
-
+    echo -e "\e[31mNginx failed to run \e[0m"
+    exit $?
+fi
+echo -e "\e[32mNginx running\e[0m"
