f6719dc86031f15bf93c043784593c5ce1e9b18c
raghav <phpngdeveloper@gmail.com>
2023-07-30 19:06:08 +0530

    code

--- functions.sh
+++ functions.sh
@@ -13,7 +13,10 @@ echo -e "\e[32$1\e[0m"
 
 function installMongo()
 {
-    cp mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/functionlogs
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+    ls /etc/yum.repos.d/
+
     yum install mongodb-org -y
     mongo --host ${mongoIp} </app/schema/${component}.js
 }
